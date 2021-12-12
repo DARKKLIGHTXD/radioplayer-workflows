5521ac572f42ccac2410f835c755dbd31bd4a458
Aayush Kumar <96004570+DARKKLIGHTXD@users.noreply.github.com>
2021-12-13 00:05:40 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -4,6 +4,10 @@ pip3 install -U pip
 sudo apt update && apt upgrade -y
 sudo apt install git curl python3-pip ffmpeg -y
 pip3 install -U pip
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install -y nodejs
+sudo apt install build-essential
+sudo npm install pm2@latest -g
 curl -sL https://deb.nodesource.com/setup_16.x | bash -
 sudo apt-get install -y nodejs
 npm i -g npm
