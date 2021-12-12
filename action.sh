9182e561926706401a5792f48f8e21300a1e3b3b
Aayush Kumar <96004570+DARKKLIGHTXD@users.noreply.github.com>
2021-12-12 22:52:03 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,14 +1,18 @@
 apt update && apt upgrade -y
 apt install git curl python3-pip ffmpeg -y
 pip3 install -U pip
-branch=master
-git clone https://github.com/AsmSafone/RadioPlayerV3.git
-RUN cd /
-RUN pip3 install -U -r requirements.txt
-RUN mkdir /AsmSafone
-WORKDIR /AsmSafone
+sudo apt update && apt upgrade -y
+sudo apt install git curl python3-pip ffmpeg -y
+pip3 install -U pip
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
+sudo apt-get install -y nodejs
+npm i -g npm
+pip3 install -U -r requirements.txt
+git clone https://github.com/AsmSafone/RadioPlayerV3.git /root/AsmSafone/ 
+cd RadioPlayerV3
+pip3 install -U -r requirements.txt
 /root/AsmSafone/ 
 cp AsmSafone/.env /root/RadioPlayerV3/.env
 cd /root/AsmSafone
-COPY start.sh /start.sh
+start.sh /start.sh
 
