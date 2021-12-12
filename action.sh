af1d2ddc6b6c75de2287972085f000bb1fce4257
Aayush Kumar <96004570+DARKKLIGHTXD@users.noreply.github.com>
2021-12-12 23:04:05 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -8,7 +8,7 @@ curl -sL https://deb.nodesource.com/setup_16.x | bash -
 sudo apt-get install -y nodejs
 npm i -g npm
 FROM debian:latest
-git clone https://github.com/AsmSafone/RadioPlayerV3.git /root/AsmSafone/ 
+git clone -b https://github.com/AsmSafone/RadioPlayerV3.git /root/AsmSafone/ 
 cd RadioPlayerV3
 pip3 install -U -r requirements.txt
 virtualenv -p /usr/bin/python3 venv 
