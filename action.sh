133e6df9e491f20651663bc7c47b7e57db2d6c98
Aayush Kumar <96004570+DARKKLIGHTXD@users.noreply.github.com>
2021-12-12 22:42:09 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -3,14 +3,12 @@ apt install git curl python3-pip ffmpeg -y
 pip3 install -U pip
 branch=master
 git clone https://github.com/AsmSafone/RadioPlayerV3.git
-requirements.txt /requirements.txt
 RUN cd /
 RUN pip3 install -U -r requirements.txt
-RUN mkdir /RadioPlayerV3
-WORKDIR /RadioPlayerV3
-/root/RadioPlayerV3/ 
-cp RadioPlayerV3/.env /root/RadioPlayerV3/.env
-cd /root/RadioPlayerV3
+RUN mkdir /AsmSafone
+WORKDIR /AsmSafone
+/root/AsmSafone/ 
+cp AsmSafone/.env /root/RadioPlayerV3/.env
+cd /root/AsmSafone
 COPY start.sh /start.sh
-FROM AsmSafone/RadioPlayerV3
 
