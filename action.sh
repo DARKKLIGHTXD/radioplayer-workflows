3ca33d0828b252682fae77e8a1ab6de707054dca
Aayush Kumar <96004570+DARKKLIGHTXD@users.noreply.github.com>
2021-12-12 22:57:33 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -7,12 +7,16 @@ pip3 install -U pip
 curl -sL https://deb.nodesource.com/setup_16.x | bash -
 sudo apt-get install -y nodejs
 npm i -g npm
-pip3 install -U -r requirements.txt
+FROM debian:latest
 git clone https://github.com/AsmSafone/RadioPlayerV3.git /root/AsmSafone/ 
 cd RadioPlayerV3
 pip3 install -U -r requirements.txt
 /root/AsmSafone/ 
-cp AsmSafone/.env /root/RadioPlayerV3/.env
-cd /root/AsmSafone
-start.sh /start.sh
+RUN cd /
+RUN pip3 install -U -r requirements.txt
+RUN mkdir /RadioPlayerV3
+WORKDIR /RadioPlayerV3
+COPY start.sh /start.sh
+CMD ["/bin/bash", "/start.sh"]
+
 
